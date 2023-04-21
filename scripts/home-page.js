967586d1788903ec48977e9dcbb1572f1c94c544
nmathurent <mathurentnelson@gmail.com>
2023-04-20 21:56:08 -0700

    Added Last Updated line

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -6,3 +6,7 @@ let currentYear = new Date().getFullYear();
 
 // Placing the value of the current year variable into the HTML file
 document.querySelector('#year').textContent = currentYear;
+
+// Determining date and time of when the current document was last modified
+let oLastModif = new Date(document.lastModified);
+document.querySelector('#lastupdated').textContent = `Last Updated: ${oLastModif.toLocaleString()}`;
