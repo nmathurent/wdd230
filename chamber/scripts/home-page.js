037c24bc2188b0091e1be5f52f55d77ebae503a1
nmathurent <mathurentnelson@gmail.com>
2023-05-10 21:16:44 -0700

    Included Today date in header

--- chamber/scripts/home-page.js
+++ chamber/scripts/home-page.js
@@ -4,9 +4,44 @@
 // Variable used to hold the current year
 let currentYear = new Date().getFullYear();
 
+// Variable used to hold the current year
+let otodayDate = new Date();
+
 // Placing the value of the current year variable into the HTML file
 document.querySelector('#year').textContent = currentYear;
 
 // Determining date and time of when the current document was last modified
 let oLastModif = new Date(document.lastModified);
 document.querySelector('#lastupdated').textContent = `Last Updated: ${oLastModif.toLocaleString().replace(',', '')}`;
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+  
+  const currentDate = new Date();
+  const monthName = months[currentDate.getMonth()];
+  const dayText = days[currentDate.getDay()];
+  const dayNum = currentDate.getDate();
+  const fullYear = currentDate.getFullYear();
+  document.querySelector('#todaydate').textContent = `${dayText}, ${dayNum} ${monthName} ${fullYear}`;
