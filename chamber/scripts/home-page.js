a694f3bad42fa12d9774d64aad1758865f8399cc
nmathurent <mathurentnelson@gmail.com>
2023-05-31 17:40:55 -0700

    Added discover html page

--- chamber/scripts/home-page.js
+++ chamber/scripts/home-page.js
@@ -101,3 +101,40 @@ if (lastVisitDate !== 0) {
 // store the last visited date
 lastVisitDate = Date.now();
 localStorage.setItem("lastvisited-date", lastVisitDate)
+
+
+// Loading Progressive images
+// Date: May 30, 2023
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+    threshold: 1,
+    rootMarging: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, imgOptions);
+
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
