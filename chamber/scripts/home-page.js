91da2d59251b7bbe119049ddce0b8c2d50bd314f
nmathurent <mathurentnelson@gmail.com>
2023-06-13 23:35:38 -0700

    Added Grid or List button

--- chamber/scripts/home-page.js
+++ chamber/scripts/home-page.js
@@ -181,7 +181,7 @@ async function getProphetData() {
 }
 
 const displayProphets = (prophets) => {
-  const cards = document.querySelector('div.cards'); // select the output container element
+  const cards = document.querySelector('article.grid'); // select the output container element
   let prophetNumber = 0;
   let prophetPosition = "";
 
@@ -232,3 +232,21 @@ const displayProphets = (prophets) => {
 
 getProphetData();
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
