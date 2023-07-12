70605433aed6dff78a756bcf9904e3d0a4cb7018
nmathurent <mathurentnelson@gmail.com>
2023-07-12 02:33:56 -0700

    Initial commit

--- bountiful/scripts/windchill.js
+++ bountiful/scripts/windchill.js
@@ -2,23 +2,20 @@
 // Date: May 23, 2023
 
 const weatherIcon = document.querySelector('#weather-icon');
+const daylist = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Irvine,CA,us&appid=cbb8d5e1cf97e51db015d685ee3d3340&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad,CA,us&appid=cbb8d5e1cf97e51db015d685ee3d3340&units=imperial';
 
 function displayResults(weatherData) {
-  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-  const desc = weatherData.weather[0].description;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png`;
+  const desc = weatherData.list[0].weather[0].description;
 
   weatherIcon.setAttribute('src', iconsrc);
   weatherIcon.setAttribute('alt', desc);
 
   // Variable used to hold the current year
-    const t = weatherData.main.temp.toFixed(0);               //   temperature in Fahrenheit
-    const s = weatherData.wind.speed;                         //   wind speed in miles per hour.
-    let  windchill = 0;
-    let  wccelsius = 0;
-    let  sexp = 0;
-    let kmperhour = s * 1.609344;            // converting mph to kph
+    const t = weatherData.list[0].main.temp.toFixed(0);               //   temperature in Fahrenheit
+    const h = weatherData.list[0].main.humidity;                      //   humidity
 
     // Placing the value of the temperature variable into the HTML file
     var elementT = document.querySelector('#temperature');
@@ -26,28 +23,60 @@ function displayResults(weatherData) {
       elementT.textContent = `${t}`;
     }
 
-    if (t <= 50 && s > 3.0) {
-      sexp = s ** 0.16
-      windchill = 34.75 + ( 0.6215 * t) - (35.75 * sexp) + (0.4275 * t * sexp);
-      windchill = windchill.toFixed(1);
-    } 
-    else {
-        windchill = "N/A";
-    }
-
-    var element = document.querySelector('#windchill');
+    var element = document.querySelector('#humidity');
     if (element) {
-      element.textContent = `${windchill}`;
+      element.textContent = `${h}`;
     }
 
-    var elementKmperhour = document.querySelector('#kmperhour');
-    if (elementKmperhour) {
-      elementKmperhour.textContent = `${kmperhour.toFixed(1)}`;
-    }
     var elementCurrentCond = document.querySelector('#currentcondition');
     if (elementCurrentCond) {
       elementCurrentCond.textContent = `${desc}`;
     }
+
+    const nd = new Date();
+    const day1 = new Date();
+    const day2 = new Date();
+    const day3 = new Date();
+    // let day1 = new Date();
+    // let day2 = new Date();
+    // let day3 = new Date();
+    day1.setDate(day1.getDate() + 1);
+    day2.setDate(day2.getDate() + 2);
+    day3.setDate(day3.getDate() + 3);
+
+    for (let i = 0; i < weatherData.list.length; i++) {
+      console.log(weatherData.list[i].dt);
+      console.log(nd.toLocaleDateString('en-CA'));
+      console.log(day1.toLocaleDateString('en-CA') + " 00:00:00");
+      console.log(day2.toLocaleDateString('en-CA') + " 00:00:00");
+      console.log(day3.toLocaleDateString('en-CA') + " 00:00:00");
+      console.log(weatherData.list[i].dt_txt);
+      console.log(weatherData.list[i].dt_txt.substr(0,10));
+      if (weatherData.list[i].dt_txt == day1.toLocaleDateString('en-CA') + " 00:00:00") {
+        var elementT = document.querySelector('#temperature1');
+        var elementD = document.querySelector('#day1');
+        if (elementT) {
+          elementT.textContent = weatherData.list[i].main.temp;
+          elementD.textContent = daylist[day1.getDay()];
+        }
+      }
+      if (weatherData.list[i].dt_txt == day2.toLocaleDateString('en-CA') + " 00:00:00") {
+        var elementT = document.querySelector('#temperature2');
+        var elementD = document.querySelector('#day2');
+        if (elementT) {
+          elementT.textContent = weatherData.list[i].main.temp;
+          elementD.textContent = daylist[day2.getDay()];
+        }
+      }
+      if (weatherData.list[i].dt_txt == day3.toLocaleDateString('en-CA') + " 00:00:00") {
+        var elementT = document.querySelector('#temperature3');
+        var elementD = document.querySelector('#day3');
+        if (elementT) {
+          elementT.textContent = weatherData.list[i].main.temp;
+          elementD.textContent = daylist[day3.getDay()];
+        }
+      }
+  }
 }
 
 async function apiFetch() {
@@ -68,37 +97,5 @@ async function apiFetch() {
       
 apiFetch();
 
-// Variable used to hold the current year
-// const t = 50;               //   temperature in Fahrenheit
-// const s = 3.1;                //   wind speed in miles per hour.
-// let  windchill = 0;
-// let  wccelsius = 0;
-// let  sexp = 0;
-// let kmperhour = s * 1.609344;            // converting mph to kph
-
-// // Placing the value of the temperature variable into the HTML file
-// var elementT = document.querySelector('#temperature');
-// if (elementT) {
-//   elementT.textContent = `${t.toFixed(0)}`;
-// }
-
-// if (t <= 50 && s > 3.0) {
-//   sexp = s ** 0.16
-//   windchill = 34.75 + ( 0.6215 * t) - (35.75 * sexp) + (0.4275 * t * sexp);
-//   windchill = windchill.toFixed(1);
-// } 
-// else {
-//      windchill = "N/A";
-// }
-
-// var element = document.querySelector('#windchill');
-// if (element) {
-//   element.textContent = `${windchill}`;
-// }
-
-// var elementKmperhour = document.querySelector('#kmperhour');
-// if (elementKmperhour) {
-//   elementKmperhour.textContent = `${kmperhour.toFixed(1)}`;
-// }
 
 
